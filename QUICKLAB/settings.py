2255eb881678d6057fd7e89e894d365f80ea16de
ehmatthes <maina@gmail.com>
2022-10-07 20:00:02 +0300

    Ready for deployment to heroku.

--- QUICKLAB/settings.py
+++ QUICKLAB/settings.py
@@ -85,25 +85,25 @@ WSGI_APPLICATION = 'QUICKLAB.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
-# DATABASE = {
+# DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Quicklab',
-#         'USER': 'Cheche',
-#         'PASSWORD':'user',
-#         'HOST': 'localhost',
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
+
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Quicklab',
+        'USER': 'Cheche',
+        'PASSWORD':'user',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
 
 
 # Password validation
